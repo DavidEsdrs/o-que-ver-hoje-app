1a85becf39f5770650e02a5b087b9ef6abc61f86
DavidEsdrs <carpina1983@gmail.com>
2024-01-13 22:39:41 -0300

    refactor: remove unnecessary import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Theme, ThemeProvider } from "styled-components"
+import { ThemeProvider } from "styled-components"
 
 const theme = {
   mainColor: "#44355B",
