0aecbb72d5fa7843cb06df88b2d0a4a8a046bcb5
DavidEsdrs <carpina1983@gmail.com>
2024-01-14 14:35:07 -0300

    refactor: SurpriseCard is a component

---
+++ src/components/SurpriseMovie/SurpriseCard.jsx
@@ -0,0 +1,110 @@
+import styled, { keyframes } from "styled-components"
+import { useRequestProcessor } from "../../hooks/useRequestProcessor"
+import { motion } from "framer-motion"
+import { server } from "../../services/api";
+
+const blink = (shadowColor) => keyframes`
+  0% {
+    box-shadow: 0 0 20px 10px ${shadowColor};
+  }
+  50% {
+    box-shadow: 0 0 10px 5px ${shadowColor};
+  }
+  100% {
+    box-shadow: 0 0 20px 10px ${shadowColor};
+  }
+`;
+
+
+const Container = styled.div`
+  width: 200px;
+  height: 300px;
+  background: linear-gradient(to bottom right, ${props => props.theme.neutralColor} 40%, ${props => props.theme.secondaryColor});
+  border-radius: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: end;
+  padding: 1rem;
+  animation: ${props => props.isLoading && blink(props.theme.secondaryColor)} 2s infinite;
+`
+
+const GenerateButton = styled.button`
+  position: relative;
+  cursor: pointer;
+  padding: 10px 15px;
+  width: 100%;
+  border-radius: 20px;
+  border: none;
+  background-color: black;
+  color: white;
+  overflow: hidden;
+  transition: 200ms;
+
+  &::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 100%;
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
+    background-color: ${props => props.theme.secondaryColor};
+    overflow: hidden;
+    transition: 200ms;
+  }
+
+  &:hover::before {
+    top: 0;
+  }
+
+  &:hover {
+    color: black;
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+`
+
+const ButtonText = styled.p`
+  position: relative;
+  z-index: 10;
+  font-size: 14px;
+  font-weight: 800;
+`
+
+export function SurpriseCard() {
+  const { query } = useRequestProcessor()
+  const randomPage = Math.floor(Math.random() * 100);
+  const randomMovie = Math.floor(Math.random() * 20);
+  const { data: movie, isLoading, isError } = query(
+    "suprise_movie",
+    () => server
+      .get(`https://api.themoviedb.org/3/discover/movie?page=${randomPage}&language=pt-BR`)
+      .then(res => res.data.results[randomMovie]),
+    { enabled: true },
+  )
+
+  if(isLoading) {
+    return (
+      <Container>
+        <GenerateButton>
+          <ButtonText disabled>
+            Surpreenda-me
+          </ButtonText>
+        </GenerateButton>
+      </Container>
+    )  
+  }
+
+  return (
+    <Container>
+      <GenerateButton>
+        <ButtonText>
+          Surpreenda-me
+        </ButtonText>
+      </GenerateButton>
+    </Container>
+  )
+}
--- src/components/SurpriseMovie/SurpriseMovie.jsx
+++ src/components/SurpriseMovie/SurpriseMovie.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { SurpriseCard } from "./SurpriseCard"
 
 const Container = styled.section`
   padding: 1rem;
@@ -15,76 +16,13 @@ const SessionTitle = styled.h3`
   margin-bottom: 1rem;
 `
 
-const SurpriseCard = styled.div`
-  width: 200px;
-  height: 300px;
-  background: linear-gradient(to bottom right, ${props => props.theme.neutralColor} 40%, ${props => props.theme.secondaryColor});
-  border-radius: 25px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: end;
-  padding: 1rem;
-`
-
-const GenerateButton = styled.button`
-  position: relative;
-  cursor: pointer;
-  padding: 10px 15px;
-  width: 100%;
-  border-radius: 20px;
-  border: none;
-  background-color: black;
-  color: white;
-  overflow: hidden;
-  transition: 200ms;
-
-  &::before {
-    content: '';
-    position: absolute;
-    left: 0;
-    top: 100%;
-    width: 100%;
-    height: 100%;
-    border-radius: 20px;
-    background-color: ${props => props.theme.secondaryColor};
-    overflow: hidden;
-    transition: 200ms;
-  }
-
-  &:hover::before {
-    top: 0;
-  }
-
-  &:hover {
-    color: black;
-  }
-
-  &:active {
-    transform: scale(0.95);
-  }
-`
-
-const ButtonText = styled.p`
-  position: relative;
-  z-index: 10;
-  font-size: 14px;
-  font-weight: 800;
-`
-
 export function SurpriseMovie() {
   return (
     <Container>
       <SessionTitle>
         Descubra um filme aleatório
       </SessionTitle>
-      <SurpriseCard>
-        <GenerateButton>
-          <ButtonText>
-            Surpreenda-me
-          </ButtonText>
-        </GenerateButton>
-      </SurpriseCard>
+      <SurpriseCard />
     </Container>
   )
 }
