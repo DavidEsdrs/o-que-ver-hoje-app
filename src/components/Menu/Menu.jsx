c676cf26bc785d2af85f0dfaa6effc31a29e2c08
DavidEsdrs <carpina1983@gmail.com>
2024-01-13 23:49:18 -0300

    feat: create basic animated menu

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+import { IoCloseSharp } from "react-icons/io5";
+import { AnimatePresence, motion } from "framer-motion"
+
+const Container = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background-color: white;
+  padding: 1rem;
+`
+
+export function Menu({ isMenuOpen, setIsMenuOpen }) {
+  return (
+    <AnimatePresence>
+      {isMenuOpen && 
+        <Container 
+          as={motion.div} 
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+        >
+          <IoCloseSharp 
+            size={24} 
+            cursor={"pointer"}
+            onClick={e => setIsMenuOpen(prev => !prev)} 
+          />
+        </Container>
+      }
+    </AnimatePresence>
+  )
+}
