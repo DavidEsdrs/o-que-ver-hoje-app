494b6310ddd559d37b7dabc5632bfe6a966a5cd3
DavidEsdrs <carpina1983@gmail.com>
2024-01-14 03:17:26 -0300

    feat: list sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ import { SurpriseMovie } from "./components/SurpriseMovie/SurpriseMovie"
 
 const Container = styled.div``
 
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
 function App() {
   return (
     <Container>
       <Header/>
-      <Recomendations />
-      <SurpriseMovie />
+      <List>
+        <Recomendations />
+        <SurpriseMovie />
+      </List>
     </Container>
   )
 }
