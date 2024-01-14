7df8703f75c7f406d71640b98264f05f02d41b27
DavidEsdrs <carpina1983@gmail.com>
2024-01-13 23:17:58 -0300

    feat: create basic mobile header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,39 @@
 import styled from "styled-components"
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Container = styled.header`
+  padding: 2rem 1rem;
+  position: relative;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  ::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 1rem;
+
+    width: calc(100% - 2rem);
+    height: 1px;
+
+    background-color: #0000002c;
+  }
+`
 
 const Title = styled.h1`
-  font-size: 1rem;
+  font-size: 1.3rem;
+  color: ${props => props.theme.mainColor};
 `
 
 export function Header() {
   return (
-    <header>
+    <Container>
       <Title>
         O que ver hoje?
-      </Title>     
-    </header>
+      </Title>
+      <GiHamburgerMenu size={24} />
+    </Container>
   )
 }
